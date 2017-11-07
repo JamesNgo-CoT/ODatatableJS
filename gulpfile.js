035f6541bbdcd34bc51fde945f2c35e5c127e14e
James Ngo <James.Ngo@toronto.ca>
2017-11-07 14:22:04 -0500

    Add gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
+const gulp = require('gulp');
 const babel = require('gulp-babel');
+const del = require('del');
 
 gulp.task('auto', () => {
-  gulp.watch('src/odatatable.js', ['odatatable']);
+  gulp.watch('src/odatatable.js', ['clean', 'odatatable']);
+});
+
+gulp.task('clean', () => {
+  del.sync(['dist']);
 });
 
 gulp.task('odatatable', () => {
-  return gulp.src('src/odatatable.js')
+  return gulp.src('src/**/*.js')
     .pipe(babel({ presets: [['env', { targets: { browsers: ['> 2% in CA', 'last 2 versions'] } }]]}))
-    .on("error", notify.onError(function(error) {
-      let msg = error.message;
-      if (msg.indexOf('Couldn\'t find preset "env"') > -1) {
-        msg += '\n\n!!!!!\nMake sure you\'ve installed the babel-preset-env npm package with: npm install babel-preset-env --save-dev\n!!!!!\n\n';
-      }
-      return msg;
-    }))
-    .pipe(gulp.dest('src/odatatable.js'));
+    .pipe(gulp.dest('dist/'));
 });
