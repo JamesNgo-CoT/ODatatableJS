b41db75cf745e38468d731c3a01139e65c763248
James Ngo <James.Ngo@toronto.ca>
2017-11-28 19:34:09 -0500

    Remove scrollX Option Default

--- dist/js/odatatable.js
+++ dist/js/odatatable.js
@@ -9,7 +9,6 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     ajax: {},
     buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
     dom: '<\'row\'<\'col-sm-6\'l><\'col-sm-6\'f>><\'row\'<\'col-sm-12\'tr>><\'row\'<\'col-sm-5\'i><\'col-sm-7\'p>>B',
-    scrollX: true,
     serverSide: true
   };
 
--- src/js/odatatable.js
+++ src/js/odatatable.js
@@ -5,7 +5,6 @@
     ajax: {},
     buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
     dom: `<'row'<'col-sm-6'l><'col-sm-6'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-5'i><'col-sm-7'p>>B`,
-    scrollX: true,
     serverSide: true
   };
 
